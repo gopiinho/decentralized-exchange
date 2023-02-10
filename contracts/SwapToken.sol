f4f95dc86b018822364bff027313db53fc3e49aa
Gurpreet <gs109690@gmail.com>
2023-02-09 23:36:08 -0600

    Single swap token contract comleted using Uniswap TransferHelper and ISwapRouter

--- contracts/SwapToken.sol
+++ contracts/SwapToken.sol
@@ -6,6 +6,70 @@ pragma abicoder v2;
 import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
-contract SwapToken {
-    constructor() {}
+contract SingleSwapToken {
+    ISwapRouter public constant swapRouter =
+        ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant BUSD = 0x4Fabb145d64652a948d72533023f6E7A623C7C53;
+
+    function swapExactInputString(
+        uint _amountIn
+    ) external returns (uint _amountOut) {
+        TransferHelper.safeTransferFrom(
+            USDC,
+            msg.sender,
+            address(this),
+            _amountIn
+        );
+        TransferHelper.safeApprove(USDC, address(swapRouter), _amountIn);
+
+        ISwapRouter.ExactInputSingleParams memory parmas = ISwapRouter
+            .ExactInputSingleParams({
+                tokenIn: USDC,
+                tokenOut: BUSD,
+                fee: 4000,
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountIn: _amountIn,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+
+        _amountOut = swapRouter.exactInputSingle(parmas);
+    }
+
+    function swapExactInputString(
+        uint _amountOut,
+        uint _amountInMaximum
+    ) external returns (uint _amountIn) {
+        TransferHelper.safeTransferFrom(
+            USDC,
+            msg.sender,
+            address(this),
+            _amountInMaximum
+        );
+        TransferHelper.safeApprove(USDC, address(swapRouter), _amountInMaximum);
+
+        ISwapRouter.ExactOutputSingleParams memory parmas = ISwapRouter
+            .ExactOutputSingleParams({
+                tokenIn: USDC,
+                tokenOut: BUSD,
+                fee: 4000,
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountOut: _amountOut,
+                amountInMaximum: _amountInMaximum,
+                sqrtPriceLimitX96: 0
+            });
+        _amountIn = swapRouter.exactOutputSingle(parmas);
+        if (_amountIn < _amountInMaximum) {
+            TransferHelper.safeApprove(USDC, address(swapRouter), 0);
+            TransferHelper.safeTransfer(
+                USDC,
+                msg.sender,
+                _amountInMaximum - _amountIn
+            );
+        }
+    }
 }
