32f131c1813ef94b2c68f8c90aa497d809e93789
Gurpreet <gs109690@gmail.com>
2023-02-10 22:54:53 -0600

    Create a weth interface to later use with testing and created SwapToken test

--- contracts/SwapToken.sol
+++ contracts/SwapToken.sol
@@ -12,6 +12,7 @@ contract SingleSwapToken {
 
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address public constant BUSD = 0x4Fabb145d64652a948d72533023f6E7A623C7C53;
+    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     function swapExactInputString(
         uint _amountIn
---
+++ test/SingleSwapToken.test.js
@@ -0,0 +1,8 @@
+const { assert, expect } = require("chai")
+const { ethers } = require("hardhat")
+
+// Assigning addresses of the coins we are about to use
+
+const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+const BUSD = "0x4Fabb145d64652a948d72533023f6E7A623C7C53"
+const WETH9 = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
