cb1e188799687b55dcb6cfdc9b0f9eea9212f7ad
Gurpreet <gs109690@gmail.com>
2023-02-10 23:57:06 -0600

    Testing - deploying the main singleswap contract inside hardhat

--- test/SingleSwapToken.test.js
+++ test/SingleSwapToken.test.js
@@ -6,3 +6,19 @@ const { ethers } = require("hardhat")
 const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
 const BUSD = "0x4Fabb145d64652a948d72533023f6E7A623C7C53"
 const WETH9 = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+
+describe("SingleSwapToken", () => {
+  let singleSwapToken
+  let accounts
+  let usdc
+  let busd
+  let weth
+
+  before(async () => {
+    accounts = await ethers.getSigners(1)
+    const swaptoken = await ethers.getContractFactory("SingleSwapToken")
+    singleSwapToken = await swaptoken.deploy()
+
+    await singleSwapToken.deployed()
+  })
+})
