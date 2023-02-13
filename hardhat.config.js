5d9b1e47d3eef8b30eb074a5fc7f8fb30b71e3f8
Gurpreet <gs109690@gmail.com>
2023-02-12 23:52:35 -0600

    Added Alchemy RPC url through env to use for testing

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-};
+  networks: {
+    hardhat: {
+      forking: {
+        url: [RINKEBY_RPC_URL],
+      },
+    },
+  },
+}
