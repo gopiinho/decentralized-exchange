426a6c8996073ccd6a1a32b19e9b875871c7d1f1
Gurpreet <gs109690@gmail.com>
2023-02-20 23:39:42 -0600

    ERC20 token 2 contract to use for exchanging

---
+++ contracts/TokenTwo.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TokenTwo is ERC20 {
+    constructor() ERC20("Token Two", "TKN2") {
+        _mint(msg.sender, 200000 * 10 ** decimals());
+    }
+}
