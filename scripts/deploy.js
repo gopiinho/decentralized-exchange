84acf22f0405a84b4564e7782064ee57bb4c7b8b
Gurpreet Singh <gs109690@gmail.com>
2023-02-22 10:02:48 -0600

    Updated deploy script with 4 contracts + added interface contract to context

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,29 +2,44 @@ const hre = require("hardhat")
 const { ethers } = require("hardhat")
 
 async function main() {
+  // SingleSwap Deploy
+  const SingleSwapToken = await hre.ethers.getContractFactory("SingleSwapToken")
+  const singleswaptoken = await SingleSwapToken.deploy()
+  await singleswaptoken.deployed()
+
+  console.log(`SingleSwap contract deployed to ${singleswaptoken.address}`)
+  console.log("----------------------------")
+
+  // MultiHop Deploy
+  const SwapMultiHop = await hre.ethers.getContractFactory("TokenOne")
+  const swapmultihop = await SwapMultiHop.deploy()
+  await swapmultihop.deployed()
+
+  console.log(`MultiHop contract deployed to ${swapmultihop.address}`)
+  console.log("----------------------------")
+
   // Token1 Deply
   const Contract1 = await hre.ethers.getContractFactory("TokenOne")
   const contract1 = await Contract1.deploy()
-
   await contract1.deployed()
 
-  console.log(`Deployed to ${contract1.address}`)
-  console.log("----------------------------")
+  console.log(`Token1 contract deployed to ${contract1.address}`)
 
   const totalSupply1 = await contract1.totalSupply()
   console.log(`Total supply: ${ethers.utils.formatUnits(totalSupply1)}`)
 
+  console.log("----------------------------")
+
   // Token1 Deply
   const Contract2 = await hre.ethers.getContractFactory("TokenTwo")
   const contract2 = await Contract2.deploy()
-
   await contract2.deployed()
 
-  console.log(`Deployed to ${contract2.address}`)
-  console.log("----------------------------")
+  console.log(`Token2 contract deployed to ${contract2.address}`)
 
   const totalSupply2 = await contract2.totalSupply()
   console.log(`Total supply: ${ethers.utils.formatUnits(totalSupply2)}`)
+  console.log("----------------------------")
 }
 
 main().catch((error) => {
