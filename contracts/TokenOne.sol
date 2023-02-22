db90e8cce358746d614aed7f5f868bd30b55cfa4
Gurpreet <gs109690@gmail.com>
2023-02-21 23:53:03 -0600

    Fixed deploy script and TokenOne

--- contracts/TokenOne.sol
+++ contracts/TokenOne.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.15;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TokenOne is ERC20 {
-    constructor() ERC20("Token Two", "TKN2") {
+    constructor() ERC20("Token One", "TKN1") {
         _mint(msg.sender, 100000 * 10 ** decimals());
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,16 +2,29 @@ const hre = require("hardhat")
 const { ethers } = require("hardhat")
 
 async function main() {
-  const Contract = await hre.ethers.getContractFactory("TokenOne")
-  const contract = await Contract.deploy()
+  // Token1 Deply
+  const Contract1 = await hre.ethers.getContractFactory("TokenOne")
+  const contract1 = await Contract1.deploy()
 
-  await contract.deployed()
+  await contract1.deployed()
 
-  console.log(`Deployed to ${contract.address}`)
+  console.log(`Deployed to ${contract1.address}`)
   console.log("----------------------------")
 
-  const totalSupply = await contract.totalSupply()
-  console.log(`Total supply: ${ethers.utils.formatUnits(totalSupply)}`)
+  const totalSupply1 = await contract1.totalSupply()
+  console.log(`Total supply: ${ethers.utils.formatUnits(totalSupply1)}`)
+
+  // Token1 Deply
+  const Contract2 = await hre.ethers.getContractFactory("TokenTwo")
+  const contract2 = await Contract2.deploy()
+
+  await contract2.deployed()
+
+  console.log(`Deployed to ${contract2.address}`)
+  console.log("----------------------------")
+
+  const totalSupply2 = await contract2.totalSupply()
+  console.log(`Total supply: ${ethers.utils.formatUnits(totalSupply2)}`)
 }
 
 main().catch((error) => {
