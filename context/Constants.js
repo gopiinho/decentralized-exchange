f18a9e152236d32cb9643a3d0d699925634be71e
Gurpreet <gs109690@gmail.com>
2023-02-22 00:31:28 -0600

    Created context folder to create ABI instances for the created contracts

---
+++ context/Constants.js
@@ -0,0 +1,13 @@
+// Importing ABI's from deployed contracts
+
+import singleSwap from "./SingleSwapToken.json"
+import multiHop from "./SwapMultiHop.json"
+import token1 from "./TokenOne.json"
+import token2 from "./TokenTwo.json"
+
+export default {
+  singleSwap,
+  multiHop,
+  token1,
+  token2,
+}
