07f464b2eab9e42f1c4edb076236a88d1f701dda
Gurpreet Singh <gs109690@gmail.com>
2023-02-22 20:05:11 -0600

    Created 2 functions to check if the wallet is connected and function to connect the wallet itself

--- utils/appFeatures.js
+++ utils/appFeatures.js
@@ -15,10 +15,28 @@ import {
 } from "../context/Constants"
 
 // Check if wallet is connected
-
 export const isWalletConnected = async () => {
   try {
     if (!window.ethereum) return console.log("Install Metamask")
+    const accounts = await window.ethereum.request({
+      method: "eth_accounts",
+    })
+    const firstAccount = accounts[0]
+    return firstAccount
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// Connect Wallet
+export const connectWallet = async () => {
+  try {
+    if (!window.ethereum) return console.log("Install Metamask")
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    })
+    const firstAccount = accounts[0]
+    return firstAccount
   } catch (error) {
     console.log(error)
   }
