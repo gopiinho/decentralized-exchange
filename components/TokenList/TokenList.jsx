083b8d024cd8c5fa16a6a17c6d5a20cfe1318dad
Gurpreet <gs109690@gmail.com>
2023-04-03 23:04:44 -0500

    Fixed token list box and its close icon

--- components/TokenList/TokenList.jsx
+++ components/TokenList/TokenList.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { IoSettingsSharp } from "react-icons/io5"
+import { AiOutlineClose } from "react-icons/ai"
 
 const TokenList = ({ tokenData, setOpenTokenBox }) => {
   // Creating empty arrat to stop the repeating of the token data
@@ -13,15 +13,15 @@ const TokenList = ({ tokenData, setOpenTokenBox }) => {
         onClick={() => setOpenTokenBox(false)}
         className="absolute cursor-pointer -right-0 mr-4 z-[11111] hover:text-pink-600 transform duration-300"
       >
-        <IoSettingsSharp size={20} />
+        <AiOutlineClose size={20} />
       </p>
       <div className="w-full py-4 font-semibold tracking-widest ">
         <h2>Your tokens</h2>
       </div>
       {tokenList.map((el, i) => (
         <div className="relative cursor-pointer border-b-2 border-white/10 py-[1px]">
-          <div className="grid grid-cols-3 items-center py-2">
-            <p className="bg-zinc-800 pr-2  border-[1px] border-pink-600 mr-11 pl-2 rounded-lg ">
+          <div className="grid grid-flow-col items-center py-3">
+            <p className="bg-zinc-800 pr-2 border border-pink-600 mr-8 pl-4 rounded-md">
               {el.symbol}
             </p>
             <p className="">
