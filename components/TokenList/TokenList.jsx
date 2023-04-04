73a3462703f955392b949ad3f2d1c47ea6cbd5fd
Gurpreet <gs109690@gmail.com>
2023-04-03 22:47:21 -0500

    Small script to stop repeating on token data

--- components/TokenList/TokenList.jsx
+++ components/TokenList/TokenList.jsx
@@ -2,6 +2,11 @@ import React, { useState } from "react"
 import { IoSettingsSharp } from "react-icons/io5"
 
 const TokenList = ({ tokenData, setOpenTokenBox }) => {
+  // Creating empty arrat to stop the repeating of the token data
+  let tokenList = []
+  for (let i = 0; i < tokenData.length; i++) {
+    if (i % 2 == 1) tokenList.push(tokenData[i])
+  }
   return (
     <div className="absolute w-80 border-[1px] border-white/20 mt-8 right-0 mr-4 p-4 bg-[#131313] rounded-xl">
       <p
@@ -13,7 +18,7 @@ const TokenList = ({ tokenData, setOpenTokenBox }) => {
       <div className="w-full py-4 font-semibold tracking-widest ">
         <h2>Your tokens</h2>
       </div>
-      {tokenData.map((el, i) => (
+      {tokenList.map((el, i) => (
         <div className="relative cursor-pointer border-b-2 border-white/10 py-[1px]">
           <div className="grid grid-cols-3 items-center py-2">
             <p className="bg-zinc-800 pr-2  border-[1px] border-pink-600 mr-11 pl-2 rounded-lg ">
