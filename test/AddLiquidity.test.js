c539baf3928e72ca68b6883cd9f677153c344a82
Gurpreet Singh <gs109690@gmail.com>
2023-04-09 23:36:11 -0500

    Test file for Liquidity contract

---
+++ test/AddLiquidity.test.js
@@ -0,0 +1,13 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+
+describe("LiquidityExamples", () => {
+    let liquidityExamples;
+    let accounts;
+    let dai;
+    let usdc;
+
+})
